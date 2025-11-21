f6a0c1319413af61abd97ee84ef72c2aae114783
jerebrrs <kevinbrrs96@gmail.com>
2025-11-21 10:30:16 -0300

    Route Login/Register/getUser terminada- (8-14)

--- ApiEcommerce/Controllers/UsersController.cs
+++ ApiEcommerce/Controllers/UsersController.cs
@@ -1,7 +1,6 @@
 using ApiEcommerce.Models.Dtos;
 using ApiEcommerce.Repository.IRepository;
 using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiEcommerce.Controllers
@@ -30,5 +29,47 @@ namespace ApiEcommerce.Controllers
             return Ok(usersDto);
         }
 
+        [HttpGet("{id:int}", Name = "GetUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetUser(int id)
+        {
+            var user = _userRepository.GetUser(id);
+            if (user == null) return NotFound($"El usuario con el id {id} no existe.");
+            var userDto = _mapper.Map<UserDto>(user);
+            return Ok(userDto);
+        }
+
+        [HttpPost(Name = "RegisterUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> RegisterUser([FromBody] CreateUserDto createUserDto)
+        {
+            if (createUserDto == null || !ModelState.IsValid) return BadRequest(ModelState);
+            if (string.IsNullOrWhiteSpace(createUserDto.Username)) return BadRequest("Username es requerido.");
+            if (!_userRepository.IsUniqueUser(createUserDto.Username)) return BadRequest("El usuario ya existe.");
+
+            var result = await _userRepository.Register(createUserDto);
+            if (result == null) return StatusCode(StatusCodes.Status500InternalServerError, "Error al registrar el");
+            return CreatedAtRoute("GetUser", new { result.Id }, result);
+        }
+
+        [HttpPost("Login", Name = "LoginUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> LoginUser([FromBody] UserLoginDto userLoginDto)
+        {
+            if (userLoginDto == null || !ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = await _userRepository.Login(userLoginDto);
+            if (user == null) return Unauthorized();
+            return Ok(user);
+        }
     }
 }
